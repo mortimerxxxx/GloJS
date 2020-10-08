765a20eec8e24e4f768973b4090720d08a10f574
Ilia Shevaldin <blacktor666@gmail.com>
2020-10-08 18:37:21 +0300

    Добавлены файлы для отслеживания

---
+++ JS/script.js
@@ -0,0 +1,10 @@
+
+const money = 10;
+const income = 5;
+const addExpenses = 15;
+const deposit = 1;
+const mission = 2;
+
+alert('Hello my first homework');
+
+console.log('Это первая легка домашка');

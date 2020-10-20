92cef27587fb13e9919e8c6e59cf60dd5a9d7ea8
Ilia Shevaldin <blacktor666@gmail.com>
2020-10-20 22:21:26 +0300

    Выполнено 9 домашнее задание

---
+++ JS/main.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let buttonStart = document.getElementById('start');
+
+let buttonIncomeAdd = document.getElementsByTagName('button')[0];
+
+let checkBox = document.querySelector('#deposit-check');
+
+let additionalIncomeItem = document.querySelectorAll('.additional_income-item');
+
+let budgetDayValue = document.getElementsByClassName('budget_day-value')[0];
+let expensesMonthValue = document.getElementsByClassName('expenses_month-value')[0];
+let additionalIncomeValue = document.getElementsByClassName('additional_income-value')[0];
+let additionalExpensesValue = document.getElementsByClassName('additional_expenses-value')[0];
+let incomePeriodValue = document.getElementsByClassName('income_period-value')[0];
+let targetMonthValue = document.getElementsByClassName('target_month-value')[0];
+
+let inputSalaryAmount = document.querySelectorAll('input')[0];
+let incomTitle = document.querySelectorAll('input')[1];
+let incomeAmount = document.querySelectorAll('input')[2];
+let expensesTitle = document.querySelectorAll('input')[5];
+let expensesAmount = document.querySelectorAll('input')[6];
+let additionalExpensesItem = document.querySelectorAll('input')[7];
+let depositCheck = document.querySelectorAll('input')[8];
+let depositAmount = document.querySelectorAll('input')[9];
+let depositPercent = document.querySelectorAll('input')[10];
+let targetAmount = document.querySelectorAll('input')[11];
+let periodSelect = document.querySelectorAll('input')[12];
+
+

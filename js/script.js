0ced98b18d679e8e5b8b10147a9afc5cff8209de
Ilia Shevaldin <blacktor666@gmail.com>
2020-11-10 20:34:22 +0300

    Домашка номер 24

--- js/script.js
+++ js/script.js
@@ -282,17 +282,18 @@ window.addEventListener('DOMContentLoaded', function () {
             const changeImage = () => {
                 let imageSrc;
 
-                document.addEventListener('mouseout', event => {
-                    if(event.target.matches('.command__photo')){
-                        event.target.src = imageSrc;
-                    }
-                    });
                 document.addEventListener('mouseover', (event) => {
                     if(event.target.matches('.command__photo')){
-                        imageSrc = event.target.src;
-                        event.target.src = event.target.dataset.img;                            
+                        event.target.src = event.target.dataset.img;
                     }
                 });
+                document.addEventListener('mouseout', event => {
+                    if(event.target.matches('.command__photo')){
+                        const datasetImg = event.target.dataset.img,
+                        startSrc = datasetImg.replace(/a(?=.jpg)/g, "");
+                        event.target.src = startSrc;     
+                    }
+                    });
             };
 
             changeImage();
@@ -300,8 +301,68 @@ window.addEventListener('DOMContentLoaded', function () {
             //ввод только чисел в калькуляторе
 
             document.addEventListener('input', (event) =>{
-                if(event.target.matches('.calc-item ')){
+                if(event.target.matches('.calc-square') || event.target.matches('.calc-day') || 
+                event.target.matches('.calc-count')){
                     event.target.value = event.target.value.replace(/\D/g, '');
                 }
             });
+
+            //калькулятор
+
+            const calc = (price = 100) =>{
+
+                const calcBlock = document.querySelector('.calc-block'),
+                    calcType = document.querySelector('.calc-type'),
+                    calcSquare = document.querySelector('.calc-square'),
+                    calcDay = document.querySelector('.calc-day'),
+                    calcCount = document.querySelector('.calc-count'),
+                    totalValue = document.getElementById('total');
+
+            const countSum = () => {
+                let total = 0,
+                    countValue = 1,
+                    dayValue = 1;
+
+                const typeValue = calcType.options[calcType.selectedIndex].value,
+                    squareValue = +calcSquare.value;
+
+
+                if(calcCount.value > 1){
+                    countValue += (calcCount.value -1) / 10;
+                }
+
+                if(calcDay.value < 5) {
+                    dayValue *= 2;
+                }else if(calcDay.value < 10){
+                    dayValue *= 1.5;
+                }
+
+                if(typeValue && squareValue) {
+                    total = price * typeValue * squareValue * countValue * dayValue;
+                }
+
+                totalValue.textContent = total;
+            };
+
+            calcBlock.addEventListener('change', (event) => {
+                const target = event.target;
+
+                // if(target.matches('.calck-type') || target.matches('calc-square') || 
+                // target.matches('.calc-day') || target.matches('.calc-count')) {
+                //     console.log(1);
+                // }
+
+                // if(target === calcType || target === calcSquare || 
+                //     target === calcDay || target === calcCount){
+                //         console.log(1);
+                //     }
+
+                if(target.matches('select') || target.matches('input')) {
+                    countSum();
+                }
+
+            });
+
+            };
+            calc(100);
         });
